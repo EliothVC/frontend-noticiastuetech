af1c8447bdb880c6875f29995b61050c257224d8
EliothVC <eliothjoaquin.vargas@alumnos.ulagos.cl>
2025-12-09 17:45:12 -0300

    Commit-2

--- src/lib/conection.ts
+++ src/lib/conection.ts
@@ -1,12 +1,17 @@
 const strapiUrl = import.meta.env.STRAPI_API_URL || 'https://app-strapi-xfml.onrender.com/api';
 
+// se usa para las imagenes
 export const STRAPI_BASE_URL = strapiUrl.replace('/api', '');
 
 export const STRAPI_URL = strapiUrl;
 
-export const fetchFromAPI = async (slag: string) => {
+//Funcion para obtener datos de la API - slug (parametro) es la ruta de la API
+export const fetchFromAPI = async (slug: string) => {
+    // Validación
     try {
-        const response = await fetch(`${STRAPI_URL}/${slag}?populate=*`);
+        // fetch a la API
+        const response = await fetch(`${STRAPI_URL}/${slug}?populate=*`);
+        // convertir a JSON
         const json = await response.json();
         // Si tiene results, es una lista, si no, es un objeto individual
         return json.data;
